df2bd936e7ede65f3ffade8bad1242da245f361d
ForeroAlexander <foreroalex26@gmail.com>
2022-02-07 18:13:41 -0500

    Deleted

--- 9-element_length.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""module doc"""
-
-from typing import Iterable, Sequence, List, Tuple
-
-
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """function docs"""
-    return [(i, len(i)) for i in lst]
