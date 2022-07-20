d73608e265702b7618504d9936f06d1ac9450306
ForeroAlexander <foreroalex26@gmail.com>
2022-07-19 23:30:43 -0500

    Main

--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 """ Main file """
-import redis
 
-get_page = __import__('web').get_page
+Cache = __import__('exercise').Cache
 
-store = redis.Redis()
+cache = Cache()
 
-URL = "http://slowwly.robertomurray.co.uk"
-count_key = "count:" + URL
-print(store.get(count_key))
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-get_page(URL)
-get_page(URL)
-get_page(URL)
-get_page(URL)
-get_page(URL)
-print(store.get(count_key))
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
